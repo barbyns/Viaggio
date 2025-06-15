e2e3980c548a892591b233514cc83388b250d2ad
Barbara <barbyns95@gmail.com>
2025-06-15 17:10:25 +0200

    change all

--- src/main/java/controller/DipendenteController.java
+++ src/main/java/controller/DipendenteController.java
@@ -1,24 +1,35 @@
-import dto.DipendenteDto;
-import entities.Dipendente;
-import mapper.DipendenteMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import repository.DipendenteRepository;
+package controller;
 
+import dto.DipendenteDto;
+import mapper.DipendenteMapper;
+import entities.Dipendente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import repository.DipendenteRepository;
 
 import java.util.List;
 
-@GetMapping
-public List<DipendenteDto> getAll() {
-    return DipendenteRepository.findAll().stream()
-            .map(DipendenteMapper::toDto)
-            .toList();
-}
+@RestController
+@RequestMapping("/dipendenti")
+public class DipendenteController {
 
-@PostMapping
-public DipendenteDto create(@RequestBody DipendenteDto dto) {
-    Dipendente d = DipendenteRepository.save(DipendenteMapper.fromDto(dto));
-    return DipendenteMapper.toDto(d);
-}
+    private final DipendenteRepository dipendenteRepository;
 
+    @Autowired
+    public DipendenteController(DipendenteRepository dipendenteRepository) {
+        this.dipendenteRepository = dipendenteRepository;
+    }
+
+    @GetMapping
+    public List<DipendenteDto> getAll() {
+        return dipendenteRepository.findAll().stream()
+                .map(DipendenteMapper::toDto)
+                .toList();
+    }
+
+    @PostMapping
+    public DipendenteDto create(@RequestBody DipendenteDto dto) {
+        Dipendente d = dipendenteRepository.save(DipendenteMapper.fromDto(dto));
+        return DipendenteMapper.toDto(d);
+    }
+}
